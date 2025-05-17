b60eefac90752d48d3f8939dec85b46375ddcdff
femke77 <megan.meyers.388@gmail.com>
2025-05-16 21:17:37 -0700

    heroku wakeup added in app

--- src/routes/api/video-routes.ts
+++ src/routes/api/video-routes.ts
@@ -6,5 +6,7 @@ const router = express.Router();
 
 router.get('/movie/:id', getVidSrcMovie);
 router.get('/tv/:series_id/:season_number/:episode_number', getVidSrcTV);
-
+router.get('/wakeup', (_req, res) => {
+  res.send('Server is awake!');
+});
 export { router as videoRouter };
